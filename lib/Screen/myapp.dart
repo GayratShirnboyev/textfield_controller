c4ba5bc449f4054484eb4c7514b508e30c777fd5
ZARIFMAC <uzbzarif@gmail.com>
2022-09-22 14:46:41 +0500

    add text button

--- lib/Screen/myapp.dart
+++ lib/Screen/myapp.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  void printTxt() {}
 
   @override
   Widget build(BuildContext context) {
     TextEditingController txt = TextEditingController(text: 'Controller');
     return MaterialApp(
       home: Scaffold(
-          body: Container(
-        child: TextField(
-          decoration: InputDecoration(labelText: 'Input'),
-          controller: txt,
-        ),
+          body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(labelText: 'Input'),
+            controller: txt,
+          ),
+          TextButton(
+            onPressed: printTxt,
+            child: Text('CLICK ME'),
+          )
+        ],
       )),
     );
   }

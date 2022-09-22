65d9d9a74803ef69517a3b107c0999369167966e
ZARIFMAC <uzbzarif@gmail.com>
2022-09-22 14:29:50 +0500

    organaze

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
+import 'Screen/myapp.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(body: Container()),
-    );
-  }
 }
